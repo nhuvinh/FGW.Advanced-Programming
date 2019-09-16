b0dc7e15e44e1a06f08144e75cff3b7207ac966c
Vinh Hoang <nhuvinh.hoang@gmail.com>
2019-09-16 13:03:23 +0700

    Re-arrange folders

---
+++ 1. OOP/AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    abstract class Tutorial
+    {
+        protected int Key;
+        public void SetKey (int key)
+        {
+            Key = key;
+        }
+
+        public int GetKey()
+        {
+            return Key;
+        }
+        public virtual void Display()
+        {
+
+        }
+    }
+    class Guru99Tutorial : Tutorial
+    {
+        protected int TutorialID;
+        protected string TutorialName;
+
+        public override void Display()
+        {
+            Console.WriteLine("Welcome to Guru99 Tutorials");
+        }
+
+        public void SetTutorial(int pID, string pName)
+        {
+            TutorialID = pID;
+            TutorialName = pName;
+        }
+
+        public String GetTutorial()
+        {
+            return TutorialName;
+        }
+
+        static void Main(string[] args)
+        {
+            Guru99Tutorial pTutor = new Guru99Tutorial();
+
+            pTutor.SetTutorial(1, ".Net");
+
+            Console.WriteLine(pTutor.GetTutorial());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 1. OOP/PrivateAccessModifier/PrivateAccessModifier/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrivateAccessModifier
+{
+    class Rectangle
+    {
+        //member variables
+        private double length;
+        private double width;
+        private string color;
+
+        private string GetColor()
+        {
+            return color;
+        }
+
+        public void Acceptdetails()
+        {
+            Console.WriteLine("Enter Length: ");
+            length = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Width: ");
+            width = Convert.ToDouble(Console.ReadLine());
+        }
+        public double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+            Console.WriteLine("Color: {0}", GetColor());
+        }
+    }//end class Rectangle
+
+    class Tabletop : Rectangle
+    {
+        public void Display()
+        {
+            base.Display();
+            Console.WriteLine("Color: {0}", GetColor());
+        }
+    }
+
+    class ExecuteRectangle
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            r.Acceptdetails();
+            r.Display();
+            Console.ReadLine();
+        }
+    }
+}
