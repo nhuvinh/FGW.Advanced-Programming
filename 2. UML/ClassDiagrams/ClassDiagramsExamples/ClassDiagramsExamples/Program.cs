03aeba85bd4ec4430bf1aa97f87bb1b6e4b9ac14
Vinh Hoang <nhuvinh.hoang@gmail.com>
2019-09-27 08:56:01 +0700

    Add code Class Diagrams

---
+++ 2. UML/ClassDiagrams/ClassDiagramsExamples/ClassDiagramsExamples/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDiagramsExamples
+{
+    class Rectangle {
+        private int width;
+        private int height;
+        double area;
+
+        public Rectangle(int w, int h) { }
+
+        public double distance (Rectangle r)
+        {
+            return 0;
+        }
+    }
+
+    class Student
+    {
+        private string name;
+        private int id;
+        private static int totalStudents;
+
+        public int getID()
+        {
+            return id;
+        }
+
+        string getEmail()
+        {
+            return name + "@fpt.edu.vn";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ 2. UML/ClassDiagrams/MovieTheater/MovieTheater/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieTheater
+{
+    class MovieTheater
+    {
+        BoxOffice BoxOffice;
+        List<Movie> Movies;
+
+        public MovieTheater()
+        {
+            BoxOffice = new BoxOffice();
+            Movies = new List<Movie>();
+        }
+
+        public void AddMovie (Movie mv)
+        {
+            Movies.Add(mv);
+        }
+
+        public bool RemoveMovie(string movieName)
+        {
+            return false;
+        }
+    }
+
+    class BoxOffice { }
+
+    class Movie { }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MovieTheater myMovieTheater = new MovieTheater();
+            Movie myMovie = new Movie();
+
+            myMovieTheater.AddMovie(myMovie);
+
+            Console.Read();
+
+        }
+    }
+}
