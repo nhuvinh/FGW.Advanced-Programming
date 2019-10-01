4effdcc52541af5c79a691ef825e9b7e5b0944c3
Vinh Hoang <nhuvinh.hoang@gmail.com>
2019-10-01 13:45:04 +0700

    Add Tiki Sequence Diagrams

---
+++ 2. UML/SequenceDiagrams/TikiSequenceDiagrams/TikiSequenceDiagrams/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TikiSequenceDiagrams
+{
+    class StoreFront {
+        public void AddItem(Item item, Cart cart)
+        {
+            cart.AddItem(item);
+        }
+
+        public bool Checkout(Cart cart, Inventory inventory)
+        {
+            cart.ProcessOrder(inventory);
+            return true;
+        }
+    }
+
+    class Cart {
+        List<Item> CartItems;
+
+        public void AddItem(Item item)
+        {
+            CartItems.Add(item);
+        }
+
+        public void ReserveItem(Inventory inventory)
+        {
+            inventory.ReserveItem(CartItems[CartItems.Count - 1]);
+        }
+
+        public void ProcessOrder(Inventory inventory)
+        {
+            foreach (var item in CartItems)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            if (ConfirmOrder())
+            {
+                PlaceOrder(inventory);
+            }
+
+            else return;
+
+        }
+
+        public void PlaceOrder(Inventory inventory)
+        {
+            inventory.PlaceOrder(CartItems);
+        }
+
+        public bool ConfirmOrder()
+        {
+            return true;
+        }
+    }
+
+    class Inventory
+    {
+        List<Item> InventoryItems;
+
+        public bool ReserveItem(Item item)
+        {
+            
+
+            if (InventoryItems.Find(r => r.ID == item.ID) != null)
+            {
+                InventoryItems.Find(r => r.ID == item.ID).Reserved();
+
+                return true;
+            }
+            else return false;
+        }
+
+        public bool PlaceOrder(List<Item> items)
+        {
+            return true;
+        }
+    }
+
+    class Item {
+        public int ID;
+        double price;
+        bool IsReserved = false;
+
+        public void Reserved()
+        {
+            IsReserved = true;
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
