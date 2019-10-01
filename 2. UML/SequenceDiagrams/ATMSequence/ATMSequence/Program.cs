872529c95085730976479b5afd691c802867266b
Vinh Hoang <nhuvinh.hoang@gmail.com>
2019-10-01 14:52:47 +0700

    Add ATM Sequence

---
+++ 2. UML/SequenceDiagrams/ATMSequence/ATMSequence/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMSequence
+{
+    class Program
+    {
+        class Customer {
+
+            public Customer()
+            {
+
+            }
+            public void InsertCard(Card card, ATM atm)
+            {
+                atm.RequestPIN();
+                int PIN = EnterPIN();
+
+                if (!atm.VerifyPIN(PIN, card)) { 
+                    Console.WriteLine("PIN Not Accepted ...");
+                } else
+                {
+                    Console.WriteLine("PIN Accepted ...");
+                    Console.WriteLine("Please choose options");
+                    Console.WriteLine("1. Deposit");
+                    Console.WriteLine("2. Withdraw");
+                }
+                
+            }
+
+            public void SelectOption(int option)
+            {
+                // To be implemented
+            }
+
+            int EnterPIN()
+            {
+                int PIN = Convert.ToInt32(Console.ReadLine());
+                return PIN;
+            }
+        }
+
+        class ATM {
+
+            public ATM()
+            {
+
+            }
+            public void RequestPIN()
+            {
+                Console.WriteLine("Please Enter Your PIN");
+                
+            }
+
+            public bool VerifyPIN (int pin, Card card)
+            {
+
+
+                return pin == card.GetAccount().GetPIN();
+            }
+        }
+
+        class Account {
+            int PIN;
+            public Account(int pin)
+            {
+                PIN = pin;
+            }
+            public int GetPIN()
+            {
+                return PIN;
+            }
+        }
+
+        class CheckAccount { }
+
+        class Card {
+
+            Account Account;
+            public Card()
+            {
+                Account = new Account(1234);
+            }
+
+
+            public Account GetAccount()
+            {
+                return Account;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Card myCard = new Card();
+            ATM myATM = new ATM();
+            Customer myCustomer = new Customer();
+
+            myCustomer.InsertCard(myCard, myATM);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2. UML/SequenceDiagrams/ATMSequence/ATMSequence/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ATMSequence")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ATMSequence")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("09f62b1f-88a4-4620-bb07-2b994684ca97")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
